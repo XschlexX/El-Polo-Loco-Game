c1a198cb1110a84dd5f42781dc68b67430934a5b
xschlexx <alexbysmith@gmail.com>
2025-06-10 19:14:10 +0200

    add classes

--- js/game.js
+++ js/game.js
@@ -2,12 +2,11 @@
 
 let canvas;
 let ctx;
-let character = new Image();
+let world = new World();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../assets/img/2_character_pepe/2_walk/W-23.png';
 
-    ctx.drawImage(character, 20, 20, 75, 150);
+    console.log('My Character is', world.character);
 }
---
+++ models/movable-objects.class.js
@@ -0,0 +1,19 @@
+class MovableObjects {
+    x = 120;
+    y = 400;
+    img;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
+    }
+}
