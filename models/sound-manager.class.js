4eee5b8b19a471a10a39a74d675af2d0ae2067fc
xschlexx <alexbysmith@gmail.com>
2025-12-02 19:51:35 +0100

    refactor(status-bar): remove commented out image arrays and cleanup code
    feat(game): add sond-manager

---
+++ models/sound-manager.class.js
@@ -0,0 +1,51 @@
+class SoundManager {
+    sounds = {}; // Hier werden alle Sounds gespeichert: { 'jump': Audio-Objekt, 'hurt': Audio-Objekt }
+
+    constructor() {
+        this.initializeSounds();
+    }
+
+    /**
+     * Lädt alle Sounds des Spiels
+     */
+    initializeSounds() {
+        // TODO: Hier fügst du die Sounds hinzu
+        // Beispiel: this.addSound('jump', 'sounds/effects/jump.mp3');
+        this.addSound('menuTheme', 'sounds/music/menu-theme.mp3');
+        this.addSound('gameTheme', 'sounds/music/game-theme.mp3');
+        this.addSound('endbossTheme', 'sounds/music/endboss-theme.mp3');
+        this.addSound('characterJump', 'sounds/effects/character-jump.mp3');
+        this.addSound('characterLand', 'sounds/effects/character-land.mp3');
+        this.addSound('characterRun', 'sounds/effects/character-run.mp3');
+        this.addSound('characterHurt', 'sounds/effects/character-hurt.wav');
+        this.addSound('coin', 'sounds/effects/coin-collect.wav');
+        this.addSound('stomp', 'sounds/effects/stomp.mp3');
+        this.addSound('background', 'sounds/background.mp3');
+    }
+
+    /**
+     * Fügt einen Sound hinzu
+     */
+    addSound(name, path) {
+        const audio = new Audio(path);
+        audio.preload = 'auto'; // Lädt Sound im Voraus
+        this.sounds[name] = audio;
+    }
+
+    /**
+     * Spielt einen Sound ab
+     */
+    play(name) {
+        if (!this.sounds[name]) {
+            console.warn(`Sound "${name}" nicht gefunden`);
+            return;
+        }
+
+        const sound = this.sounds[name];
+        sound.currentTime = 0; // Startet von vorne
+        sound.play().catch(err => {
+            // Browser blockiert manchmal Autoplay
+            console.warn(`Fehler beim Abspielen von "${name}":`, err);
+        });
+    }
+}
--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,31 +1,4 @@
 class StatusBar extends DrawableObject {
-    // imagesHealth = [
-    //     '../assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
-    //     '../assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-    //     '../assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-    //     '../assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-    //     '../assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-    //     '../assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
-    // ];
-
-    // imagesBottle = [
-    //     '../assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
-    //     '../assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
-    //     '../assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
-    //     '../assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
-    //     '../assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
-    //     '../assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
-    // ];
-
-    // imagesCoin = [
-    //     '../assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
-    //     '../assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
-    //     '../assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
-    //     '../assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
-    //     '../assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
-    //     '../assets/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
-    // ];
-
     imagesHealthBar = [
         '../assets/img/7_statusbars/4_bar_elements/statusbar_empty_modified.png',
         '../assets/img/7_statusbars/4_bar_elements/statusbar_blue_modified.png',
